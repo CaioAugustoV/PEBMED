ade84ce0f12de04967753209dec03947b0d362c4
Caio Augusto <caioaugustov@gmail.com>
2021-02-08 21:22:36 -0300

    Configuration React Navigation and organization to project

--- App.js
+++ App.js
@@ -1,27 +1,7 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import Router from './src/routes'
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+const App = () => <Router />;
 
 export default App;
